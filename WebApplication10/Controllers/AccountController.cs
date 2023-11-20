5d26a88781c49edce1fb5144d290063abd711ef9
JPolakow <Jonathan@Polakow.co.za>
2023-11-20 15:30:23 +0200

    Added title

--- WebApplication10/Controllers/AccountController.cs
+++ WebApplication10/Controllers/AccountController.cs
@@ -19,7 +19,6 @@ public class AccountController : Controller
       await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
    }
 
-   [ValidateAntiForgeryToken]
    [Authorize]
    public async Task Logout()
    {
@@ -33,7 +32,6 @@ public class AccountController : Controller
       await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
    }
 
-   [ValidateAntiForgeryToken]
    [Authorize]
    public IActionResult Profile()
    {
